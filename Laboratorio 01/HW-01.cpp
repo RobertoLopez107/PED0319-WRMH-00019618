374d2aabed5835ccee97e44c7577352329cf58bf
Walter26 <walterrafael26@gmail.com>
2020-01-12 16:13:28 -0600

    Mod: readme for labo 01

--- Laboratorio 01/HW-01.cpp
+++ Laboratorio 01/HW-01.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+bool isPrime(int n){
+    
+}
+
+int addPrimes(void){
+    int sum = 0;
+    for(int i = 2; i <= 100; i++){
+        if(isPrime(i) == true)
+            sum += i;
+    }
+
+    return sum;
+}
